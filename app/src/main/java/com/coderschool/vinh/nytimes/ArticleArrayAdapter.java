5ad7924b557c87d71f72b4b3d381f6c7f8ab9ba3
ntvinh11586 <ntvinh.11586@gmail.com>
2016-10-20 21:53:21 +0700

    Add Gilde

--- app/src/main/java/com/coderschool/vinh/nytimes/ArticleArrayAdapter.java
+++ app/src/main/java/com/coderschool/vinh/nytimes/ArticleArrayAdapter.java
@@ -13,7 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -85,7 +85,7 @@ public class ArticleArrayAdapter extends
         String thumbnail = contact.getThumbNail();
 
         if (!TextUtils.isEmpty(thumbnail)) {
-            Picasso.with(getContext()).load(thumbnail).into(viewHolder.ivImage);
+            Glide.with(getContext()).load(thumbnail).into(viewHolder.ivImage);
         }
     }
 
